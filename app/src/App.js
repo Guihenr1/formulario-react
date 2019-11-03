7bc1d482c4a38fc7f92227bacee1dad24f04f992
Guilherme Pompilio <guilherme.pompilio@digi.ag>
2019-11-03 16:25:32 -0300

    Inicio projeto React

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserForm from './pages/user/UserForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserForm></UserForm>
+    </div>
+  );
+}
+
+export default App;
